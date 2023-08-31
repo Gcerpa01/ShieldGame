40c76a5fccd73fd03e2883f36ccddcda6b6f3cfe
Gerardo <gcerpa02@cox.net>
2023-08-31 13:50:08 -0700

    Adding Player and initial jump

---
+++ Assets/player_movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0,5,0);
+        }
+    }
+}
