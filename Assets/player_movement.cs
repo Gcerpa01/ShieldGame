639329e1bf9cfe2ed75bfa6152ce303de00a3a6c
Gerardo <gcerpa02@cox.net>
2023-09-07 16:15:47 -0700

    Added player movement

--- Assets/player_movement.cs
+++ Assets/player_movement.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Animator anim;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown("space"))
+        float xDir = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(xDir*8.5f,rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0,5,0);
+            rb.velocity = new Vector2(rb.velocity.x,7);
         }
+
+        if (xDir > 0){
+            anim.SetBool("isRunning",true);
+        }
+        else if (xDir < 0){
+            anim.SetBool("isRunning",true);
+        }
+        
+        else anim.SetBool("isRunning",false);
+
     }
 }
