70dcd8054369df05e7afdb048ba7d51c2c2397ec
Gerardo <gcerpa02@cox.net>
2023-09-10 14:12:33 -0700

    Added shield throw

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -7,13 +7,30 @@ public class Weapon : MonoBehaviour
     public Transform launchPoint;
     public GameObject shieldPrefab;
 
+    private enum MovementState {thrown,stop,release};
+    
+    MovementState shieldState = MovementState.thrown;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) throwShield();
+        if (Input.GetButtonDown("Fire1")) {
+            throwShield();
+        }
+
     }
 
     void throwShield(){
-        Instantiate(shieldPrefab,launchPoint.position,launchPoint.rotation);
+        switch(shieldState){
+            case MovementState.thrown:
+                Instantiate(shieldPrefab,launchPoint.position,launchPoint.rotation);
+                shieldState = MovementState.stop;
+                break;
+            case MovementState.stop:
+                shieldState = MovementState.release;
+                break;
+            case MovementState.release:
+                shieldState = MovementState.thrown;
+                break;
+        }
     }
 }
--- Assets/shieldLogic.cs
+++ Assets/shieldLogic.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class shieldLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 13.8f;
+    public Rigidbody2D rb;
+    private enum MovementState {thrown, stop, release};
+    MovementState shieldState = MovementState.thrown;
+
     void Start()
-    {   
+    {
+        // playerMovement = GetComponent<player_movement>();
+        
+        // rb.velocity = playerMovement.IsFacingRight? transform.right * speed : transform.right * speed * -1;
+        rb.velocity = transform.right * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            switch (shieldState)
+            {
+                case MovementState.thrown:
+                    rb.velocity = Vector2.zero;
+                    shieldState = MovementState.stop;
+                    break;
+                case MovementState.stop:
+                    Destroy(gameObject);
+                    break;
+            }
+        }
     }
 }
